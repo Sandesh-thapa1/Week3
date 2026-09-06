43a7421bad79b21c342a963b671425c5a278cfd2
Sandesh-thapa1 <sandest@metropolia.fi>
2026-09-06 17:19:47 +0300

    Add CORS support for frontend testing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 import api from './api/index.js';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api/v1', api);
+
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
